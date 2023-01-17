ca9af1febf84febe2c1714ea66fa358f5f60037a
Ikhsan <xsanhakim14@gmail.com>
2023-01-17 11:37:39 +0700

    Belum selesai

--- src/UAS.java
+++ src/UAS.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class UAS {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class UAS {
+    public static void main(String[] args) {
+        int jum=4;
+        UAS1D myMessage = new UAS1D();
+        Scanner sc = new Scanner(System.in);
+        myMessage.setMessageID(sc.nextLine());
+        myMessage.setMessageName(sc.nextLine());
+        ArrayList<String> context = new ArrayList<>();
+        for(int index=0;index<jum;index++){
+            context.add(sc.nextLine());
+        }
+        myMessage.setContext(context);
+        System.out.println("Kata yang dicari : ");
+        String cari = sc.nextLine();
+        System.out.println(myMessage.FindWords(context,cari));
+    }
 }
--- src/UAS1D.java
+++ src/UAS1D.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class UAS1D {
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class UAS1D {
+    public String getMessageID() {
+        return MessageID;
+    }
+
+    public void setMessageID(String messageID) {
+        MessageID = messageID;
+    }
+
+    public String getMessageName() {
+        return MessageName;
+    }
+
+    public void setMessageName(String messageName) {
+        MessageName = messageName;
+    }
+
+    public ArrayList<String> getContext() {
+        return Context;
+    }
+
+    public void setContext(ArrayList<String> context) {
+        Context = context;
+    }
+
+    private String MessageID;
+    private String MessageName;
+    private ArrayList<String> Context;
+    public Objects FindWords(ArrayList<String> conteks, String cari) {
+        for(String ok: conteks){
+            if(ok.contains(cari)){
+                System.out.println("Indeks ke "+ cari);
+            }
+        }
+        return conteks.indexOf(cari);
+    }
+
+
+    public UAS1D(){}
+   public UAS1D(String id,String name, ArrayList<String> conteks){
+        this.MessageID=id;
+        this.MessageName=name;
+        this.Context=conteks;
+
+
+    }
+
+
+
 }
